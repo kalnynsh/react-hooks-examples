13f2dcda92929b44baf5dba6aaf7dbcb9d7becf0
kalnynsh <kda869@yandex.ru>
2020-04-23 19:23:46 +0300

    Added HookSwitcher.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const App = () => {
+  return (
+    <HookSwitcher />
+  );
+};
+
+const HookSwitcher = () => {
+
+  const [ bgColor, setBgColor ] = useState('white');
+  const [ fontSize, setFontSize] = useState(14);
+
+  return (
+    <div
+      style={{ padding: '5%', backgroundColor: bgColor, width: '33%' }}
+    >
+      <button
+        style={{marginRight: '2%', fontSize: `${fontSize}px`}}
+        onClick={ () => setBgColor('blue') }
+      >
+        Blue
+      </button>
+      <button
+        style={{marginRight: '2%', fontSize: `${fontSize}px`}}
+        onClick={ () => setBgColor('gray') }
+      >
+        Gray
+      </button>
+      <button
+        style={{marginRight: '2%', fontSize: `${fontSize}px`}}
+        onClick={ () => setFontSize((s) => s + 2)}
+      >
+        Font size up
+      </button>
+      <button
+        style={{fontSize: `${fontSize}px`}}
+        onClick={ () => setFontSize((s) => s - 2)}
+      >
+        Font size down
+      </button>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
